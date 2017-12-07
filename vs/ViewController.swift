e679bf1772cd0ee22dc013cfe2f861657470df52
Luigi De Marco <gigi.demarco@hotmail.it>
2017-12-07 09:52:11 +0100

    Initial Commit

---
+++ vs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vs
+//
+//  Created by Luigi De Marco on 07/12/2017.
+//  Copyright © 2017 Luigi De Marco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
